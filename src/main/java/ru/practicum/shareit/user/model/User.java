1011c85f3c0da9f722dad38b4f6d7de4837ece43
klimov.roman.vit <klimov.roman.vit@yandex.ru>
2023-09-25 18:03:16 +0300

    13tz 5/1it

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,8 +1,5 @@
 package ru.practicum.shareit.user.model;
-
 import lombok.Data;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
 
 @Data
 public class User {
